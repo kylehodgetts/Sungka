6cc813d60134e30b342d37b771cc75740f2d5f28
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-23 15:40:26 +0100

    Created new layout and list view row. Connected main activity to connect activity

---
+++ app/src/main/java/com/kylehodgetts/sunka/ConnectActivity.java
@@ -0,0 +1,17 @@
+package com.kylehodgetts.sunka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kylehodgetts on 23/10/2015.
+ */
+public class ConnectActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
