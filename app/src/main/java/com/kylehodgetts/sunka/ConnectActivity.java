76abb2b6704c87e7f547e14b4a4ed04a10a07ccb
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-23 16:24:00 +0100

    First attempt at socket connection

--- app/src/main/java/com/kylehodgetts/sunka/ConnectActivity.java
+++ app/src/main/java/com/kylehodgetts/sunka/ConnectActivity.java
@@ -1,11 +1,14 @@
 package com.kylehodgetts.sunka;
 
 import android.app.Activity;
-import android.net.wifi.p2p.WifiP2pDevice;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.util.ArrayList;
 
 /**
@@ -19,6 +22,10 @@ public class ConnectActivity extends Activity {
     private PeerListAdapter peerListAdapter;
     private ArrayList<String> devices;
 
+    private SocketReceiver socketReciever;
+    private MulticastSocket multicastSocket;
+    private InetAddress group;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +35,50 @@ public class ConnectActivity extends Activity {
         devices = new ArrayList<String>();
         peerListAdapter = new PeerListAdapter(this, devices);
         peerList.setAdapter(peerListAdapter);
-
+        socketReciever = new SocketReceiver();
+        socketReciever.execute();
     }
 
     // Create a find peers task
+    class SocketReceiver extends AsyncTask<Void, String, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            try {
+                multicastSocket = new MulticastSocket(2222);
+                group = InetAddress.getByName("225.4.5.6");
+                multicastSocket.joinGroup(group);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                DatagramPacket packet;
+                while(!isCancelled()) {
+                     byte[] buf = new byte[256];
+                    packet = new DatagramPacket(buf, buf.length);
+                    multicastSocket.receive(packet);
+                    publishProgress(packet.getData().toString());
+                }
+                multicastSocket.leaveGroup(group);
+                multicastSocket.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            devices.add(values[0]);
+            peerListAdapter.notifyDataSetChanged();
+            return;
+        }
+    }
 }
