f8242765f4e1a314c9cc799cff678b7014a87225
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-11-06 18:04:57 +0000

    SingletonSocket

---
+++ app/src/main/java/com/kylehodgetts/sunka/SingletonSocket.java
@@ -0,0 +1,19 @@
+package com.kylehodgetts.sunka;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by kylehodgetts on 06/11/2015.
+ */
+public class SingletonSocket {
+    private static Socket socket;
+
+    public static synchronized Socket getSocket(){
+        return socket;
+    }
+
+    public static synchronized void setSocket(Socket socket){
+        SingletonSocket.socket = socket;
+    }
+}
