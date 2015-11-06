c8edc8ec380c9f5cedbed708c400cd4b9be5ae7f
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-11-06 12:33:52 +0000

    Setup static host activity

---
+++ app/src/main/java/com/kylehodgetts/sunka/HostActivity.java
@@ -0,0 +1,127 @@
+package com.kylehodgetts.sunka;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by kylehodgetts on 06/11/2015.
+ */
+public class HostActivity extends Activity {
+    private TextView txtAddress;
+    private TextView txtPort;
+    private TextView txtStatus;
+
+    private ServerSocket serverSocket;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+
+        txtAddress = (TextView) findViewById(R.id.txtHostAddress);
+        txtPort = (TextView) findViewById(R.id.txtHostPort);
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+
+        txtAddress.setText(getIpAddress());
+
+        Thread serverSocketThread = new Thread(new SocketServerThread());
+        serverSocketThread.start();
+    }
+
+    private class SocketServerThread extends Thread {
+        static final int PORT = 8080;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(PORT);
+                HostActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtPort.setText(serverSocket.getLocalPort());
+                    }
+                });
+
+                while(true) {
+                    final Socket socket = serverSocket.accept();
+                    HostActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtStatus.setText("Connected from " + socket.getInetAddress() + ":" + socket.getPort());
+                        }
+
+                        SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(socket);
+                    });
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerReplyThread extends Thread {
+     private Socket hostThreadSocket;
+
+        public SocketServerReplyThread(Socket hostThreadSocket) {
+            this.hostThreadSocket = hostThreadSocket;
+        }
+
+        @Override
+        public void run() {
+            OutputStream outputStream;
+            final String message = "I AM HERE";
+            try{
+                outputStream = hostThreadSocket.getOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                printStream.print(message);
+                printStream.close();
+
+                HostActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtStatus.setText(txtStatus.getText() + " " + message);
+                    }
+                });
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try{
+            Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+            while(enumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+                Enumeration<InetAddress> enumInet = networkInterface.getInetAddresses();
+
+                while(enumInet.hasMoreElements()) {
+                    InetAddress inetAddress = enumInet.nextElement();
+
+                    if(inetAddress.isSiteLocalAddress()) {
+                        ip += "Site Local Address: " + inetAddress.getHostAddress() + "\n";
+                    }
+                }
+            }
+        }
+        catch(SocketException e) {
+            e.printStackTrace();
+        }
+
+        return ip;
+    }
+}
--- app/src/main/java/com/kylehodgetts/sunka/WiFiDirectActivity.java
+++ app/src/main/java/com/kylehodgetts/sunka/WiFiDirectActivity.java
@@ -32,6 +32,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -45,171 +47,42 @@ import java.util.List;
  * @version 1.0
  * @see {@link "developer.android.com/training/connect-devices-wirelessly/wifi-direct.html"}
  */
-public class WiFiDirectActivity extends Activity implements ChannelListener {
-
+public class WiFiDirectActivity extends Activity {
     public static final String TAG = "wifidirectactivity";
-    private ListView peerList;
-    private ArrayList<WifiP2pDevice> arrayListPeers;
-    private PeerListAdapter peerListAdapter;
 
-    private WifiP2pManager manager;
-    private boolean isWifiP2pEnabled = false;
-    private boolean retryChannel = false;
-
-    private final IntentFilter intentFilter = new IntentFilter();
-    private Channel channel;
-    private BroadcastReceiver receiver = null;
-
-    /**
-     * @param isWifiP2pEnabled the isWifiP2pEnabled to set
-     */
-    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
-        this.isWifiP2pEnabled = isWifiP2pEnabled;
-    }
-
+    private EditText editAddress;
+    private EditText editPort;
+    private Button btnConnect;
+    private Button btnHost;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifidirect);
-        arrayListPeers = new ArrayList<>();
-        peerListAdapter = new PeerListAdapter(this, arrayListPeers);
-        peerList = (ListView) findViewById(R.id.peerList);
-        peerList.setAdapter(peerListAdapter);
-        peerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        editAddress = (EditText) findViewById(R.id.editTextAddress);
+        editPort = (EditText) findViewById(R.id.editTextPort);
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnHost = (Button) findViewById(R.id.btnHost);
+        btnHost.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Initiate connections
-                Toast.makeText(WiFiDirectActivity.this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                startActivity(new Intent(WiFiDirectActivity.this, HostActivity.class));
             }
         });
 
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
-        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-
-            @Override
-            public void onSuccess() {
-
-            }
-
-            @Override
-            public void onFailure(int reason) {
-
-            }
-        });
     }
 
     /** register the BroadcastReceiver with the intent values to be matched */
     @Override
     public void onResume() {
         super.onResume();
-        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
-        registerReceiver(receiver, intentFilter);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        unregisterReceiver(receiver);
     }
 
-    public void connect(WifiP2pConfig config) {
-        manager.connect(channel, config, new ActionListener() {
-
-            @Override
-            public void onSuccess() {
-                // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
-            }
-
-            @Override
-            public void onFailure(int reason) {
-                Toast.makeText(WiFiDirectActivity.this, "Connect failed. Retry.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    public void onChannelDisconnected() {}
-
-    public void resetData() {}
-
-    public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
-
-        private WifiP2pManager manager;
-        private Channel channel;
-        private WiFiDirectActivity activity;
-
-        private List listPeers = new ArrayList();
-        private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
-            @Override
-            public void onPeersAvailable(WifiP2pDeviceList peers) {
-                arrayListPeers.clear();
-                arrayListPeers.addAll(peers.getDeviceList());
-                peerListAdapter.notifyDataSetChanged();
-                if (arrayListPeers.size() == 0) {
-                    Log.d(WiFiDirectActivity.TAG, "No devices found");
-                    return;
-                }
-            }
-        };
 
 
-        /**
-         * @param manager WifiP2pManager system service
-         * @param channel Wifi p2p channel
-         * @param activity activity associated with the receiver
-         */
-        public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
-                                           WiFiDirectActivity activity) {
-            super();
-            this.manager = manager;
-            this.channel = channel;
-            this.activity = activity;
-        }
-
-        /*
-         * (non-Javadoc)
-         * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
-         * android.content.Intent)
-         */
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-
-                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                    activity.setIsWifiP2pEnabled(true);
-                } else {
-                    activity.setIsWifiP2pEnabled(false);
-                    activity.resetData();
-
-                }
-                Log.d(WiFiDirectActivity.TAG, "P2P state changed - " + state);
-            }
-            else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-                if (manager != null) {
-                    manager.requestPeers(channel, peerListListener);
-                }
-                Log.d(WiFiDirectActivity.TAG, "P2P peers changed");
-            }
-            else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-
-            }
-            else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-//            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
-//                    .findFragmentById(R.id.frag_list);
-//            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
-//                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
-
-            }
-        }
-
-    }
 }
