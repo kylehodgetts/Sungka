1bd5777b35ce1248aa7b167cbc9449a2d533cc73
Charlie Baker <Charlie.Baker@kcl.ac.uk>
2015-11-09 13:06:21 +0000

    Created a new class to draw a shell and asign it a random colour

---
+++ app/src/main/java/com/kylehodgetts/sunka/uiutil/ShellDrawable.java
@@ -0,0 +1,83 @@
+package com.kylehodgetts.sunka.uiutil;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.view.View;
+
+import java.util.Random;
+
+/**
+ * Created by CBaker on 08/11/2015.
+ */
+public class ShellDrawable extends View {
+
+    public static String[] colours = {"#00FF33", "#0066CC", "#00FF99", "#660099", "#CCFF00", "#FF6600", "#FF0099"};
+
+    private ShapeDrawable shell;
+    private int x;
+    private int y;
+    private int width;
+    private int length;
+
+
+    public ShellDrawable(Context context, int x, int y, int width, int length) {
+        super(context);
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.length = length;
+
+        shell = new ShapeDrawable(new OvalShape());
+        shell.setBounds(x, y, x + width, y + length);
+
+        Random random = new Random();
+        shell.getPaint().setColor(Color.parseColor(colours[random.nextInt(colours.length)]));  //sets colour randomly
+
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getShellX() {
+        return x;
+    }
+
+    public void setShellX(int x) {
+        this.x = x;
+    }
+
+    public int getShellWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getShellY() {
+        return y;
+    }
+
+    public void setShellY(int y) {
+        this.y = y;
+    }
+
+    public void setColour(String newColour) {
+        this.shell.getPaint().setColor(Color.parseColor(newColour));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        shell.draw(canvas);
+        canvas.drawOval(x, y, width, length, shell.getPaint());
+    }
+}
---
+++ app/src/test/java/com/kylehodgetts/sunka/controller/AnimationManagerTest.java
@@ -0,0 +1,50 @@
+package com.kylehodgetts.sunka.controller;
+
+import com.kylehodgetts.sunka.BoardActivity;
+import com.kylehodgetts.sunka.controller.bus.EventBus;
+import com.kylehodgetts.sunka.event.ShellMovement;
+import com.kylehodgetts.sunka.model.Board;
+import com.kylehodgetts.sunka.model.GameState;
+import com.kylehodgetts.sunka.model.Player;
+import com.kylehodgetts.sunka.util.Tuple2;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by kylehodgetts on 14/11/2015.
+ */
+public class AnimationManagerTest extends TestCase {
+
+    private EventBus bus;
+    private GameState gameState;
+    private AnimationManager animationManager;
+    private BoardActivity boardActivity;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        gameState = new GameState(new Board(), new Player(), new Player());
+        boardActivity = new BoardActivity();
+        bus = new EventBus(gameState, boardActivity);
+        animationManager = new AnimationManager(bus, boardActivity);
+
+    }
+
+    public void testPreConditions() throws Exception {
+        assertNotNull(gameState);
+        assertNotNull(boardActivity);
+        assertNotNull(bus);
+        assertNotNull(animationManager);
+    }
+
+    public void testHandleEvent_shellMovement() throws Exception {
+        Tuple2 tuple2 = animationManager.handleEvent(new ShellMovement(0, 0, 2, false, 0))
+    }
+
+    public void testHandleEvent_shellMovementToPot() throws Exception {
+        assertTrue(true);
+    }
+
+    public void testHandleEvent_shellSteal() throws Exception {
+        assertTrue(true);
+    }
+}
