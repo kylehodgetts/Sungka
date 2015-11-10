c6ef94744439c5692101ff36e305666f124fbf0d
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-11-10 19:19:16 +0000

    New test file for host activity

---
+++ app/src/androidTest/java/com/kylehodgetts/sunka/HostActivityTest.java
@@ -0,0 +1,29 @@
+package com.kylehodgetts.sunka;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by kylehodgetts on 10/11/2015.
+ */
+public class HostActivityTest extends ActivityInstrumentationTestCase2<HostActivity> {
+
+    public HostActivityTest() {
+        super(HostActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testRegisterService() throws Exception {
+
+    }
+
+    public void testInitialiseRegistrationListener() throws Exception {
+
+    }
+}
