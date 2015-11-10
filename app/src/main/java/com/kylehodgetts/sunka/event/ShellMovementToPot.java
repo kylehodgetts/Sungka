35ed129a87c9f6347f1e0191fc184325c8214919
Jonathan Burton <jd_burton@hotmail.co.uk>
2015-11-10 16:33:32 +0000

    Added an event to signifying moving shells to the pot

---
+++ app/src/main/java/com/kylehodgetts/sunka/event/ShellMovementToPot.java
@@ -0,0 +1,34 @@
+package com.kylehodgetts.sunka.event;
+
+import com.kylehodgetts.sunka.controller.bus.Event;
+
+/**
+ * @author Jonathan
+ * @version 1.0
+ *          <p/>
+ *          Event that notifies that shells are about to be placed in pot
+ */
+public class ShellMovementToPot implements Event {
+
+    private int playerPotIndex, nextPlayerIndex, shells;
+
+    public ShellMovementToPot(int playerPotIndex, int shellsStillToBePlaced) {
+        this.playerPotIndex = playerPotIndex;
+        this.nextPlayerIndex = (playerPotIndex + 1) % 2;
+        this.shells = shellsStillToBePlaced;
+    }
+
+    public int getShells() {
+        return shells;
+    }
+
+    public int getPlayerPotIndex() {
+        return playerPotIndex;
+    }
+
+    public int getNextPlayerIndex() {
+        return nextPlayerIndex;
+    }
+
+
+}
