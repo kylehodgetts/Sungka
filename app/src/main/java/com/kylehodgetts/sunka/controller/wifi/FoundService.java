8f3383e44779a4fb3a3cda1f20d61ffc215edd1b
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-11-12 14:01:44 +0000

    FoundService documented

--- app/src/main/java/com/kylehodgetts/sunka/controller/wifi/FoundService.java
+++ app/src/main/java/com/kylehodgetts/sunka/controller/wifi/FoundService.java
@@ -5,7 +5,7 @@ import java.net.InetAddress;
 /**
  * @author Adam Chlupacek
  * @version 1.0
- *          <-INPUT DESC->
+ * Contains the name, address and port of a found service
  */
 public class FoundService {
 
@@ -13,22 +13,38 @@ public class FoundService {
     private InetAddress address;
     private int port;
 
-
+    /**
+     * Constructs a new found service
+     * @param name      The service's name
+     * @param address   The service's <code>InetAddress</code>
+     * @param port      The service's registered port
+     */
     public FoundService(String name, InetAddress address, int port) {
         this.name = name;
         this.address = address;
         this.port = port;
     }
-
-
+    
+    /**
+     *
+     * @return The service name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return The service address
+     */
     public InetAddress getAddress() {
         return address;
     }
 
+    /**
+     *
+     * @return The service port
+     */
     public int getPort() {
         return port;
     }
