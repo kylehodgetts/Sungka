e8ccd67d2e6da53c35043af8a6c10330c9f91ffc
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-11-15 12:37:50 +0000

    Documented Fonts Test

--- app/src/androidTest/java/com/kylehodgetts/sunka/uiutil/FontsTest.java
+++ app/src/androidTest/java/com/kylehodgetts/sunka/uiutil/FontsTest.java
@@ -4,26 +4,31 @@ import android.graphics.Typeface;
 import android.test.AndroidTestCase;
 import android.test.mock.MockContext;
 
+import org.junit.Test;
+
 /**
  * @author Kyle Hodgetts
  * @version 1.0
  * Responsible for testing the <code>Fonts</code> utility class
  */
 public class FontsTest extends AndroidTestCase {
-    private MockContext context;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        context = new MockContext();
-    }
-
+    /**
+     * Asserts that the correct font is returned
+     * @throws Exception
+     */
+    @Test
     public void testGetTitleFont() throws Exception {
         Typeface expected = Typeface.createFromAsset(getContext().getAssets(), String.format("fonts/%s", "fordscript.ttf"));
         Typeface actual = Fonts.getTitleFont(getContext());
         assertEquals(expected.getStyle(), actual.getStyle());
     }
 
+    /**
+     * Asserts that the correct font is returned
+     * @throws Exception
+     */
+    @Test
     public void testGetButtonFont() throws Exception {
         Typeface expected = Typeface.createFromAsset(getContext().getAssets(), String.format("fonts/%s", "fordscript.ttf"));
         Typeface actual = Fonts.getTitleFont(getContext());
