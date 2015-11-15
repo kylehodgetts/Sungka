0bc97526f5ea5d27816a1431095c4805c8015e82
Jonathan Burton <jd_burton@hotmail.co.uk>
2015-11-15 22:07:45 +0000

    Added most tests for AIStrategy class, all pass. This required making changes in the AIStrategy class so I could have access to the methods and also some edits to the defendAgainstAnotherTurn so it actually did as it was meant to

---
+++ app/src/test/java/com/kylehodgetts/sunka/controller/AI/AIStrategyTest.java
@@ -0,0 +1,121 @@
+package com.kylehodgetts.sunka.controller.AI;
+
+import com.kylehodgetts.sunka.model.Board;
+import com.kylehodgetts.sunka.util.Tuple2;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * @author Jonathan Burton
+ * @version 1.0
+ */
+public class AIStrategyTest extends AIStrategy {
+
+    Tuple2<Boolean, Integer> result;
+    private AIStrategy ai;
+    private Board board;
+
+    @Before
+    public void setUp() {
+        ai = new AIStrategy();
+        board = new Board();
+    }
+
+    /**
+     * Tests if the ai can see and execute getting another turn
+     */
+    @Test
+    public void testCanPlayerGetAnotherTurn() {
+        for (int tray = 0; tray < 7; tray++) {
+            board = new Board();
+            board.setTray(AI.PLAYER_AI, tray, 7 - tray);
+            result = ai.canPlayerGetAnotherTurn(AI.PLAYER_AI, board);
+            Assert.assertEquals((int) result.getY(), tray);
+        }
+    }
+
+    /**
+     * This test confirms the AI can perform the capture rule
+     */
+    @Test
+    public void testCanPlayerPerformCaptureRule() {
+        for (int emptyTray = 1; emptyTray < 7; emptyTray++) {
+            for (int trayToChoose = 0; trayToChoose < 7; trayToChoose++) {
+                board = new Board();
+
+                board.setTray(AI.PLAYER_AI, emptyTray, 0);
+                board.setTray(AI.PLAYER_AI, trayToChoose, emptyTray - trayToChoose);
+                result = ai.canPlayerPerformCaptureRule(AI.PLAYER_AI, board);
+
+                if (trayToChoose < emptyTray) {
+                    Assert.assertTrue(result.getX());
+                    Assert.assertEquals((int) result.getY(), trayToChoose);
+                } else {
+                    Assert.assertFalse(result.getX());
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testDefendAgainstCaptureRule() {
+
+
+    }
+
+    /**
+     * Tests that the ai can determine if the other player can get a turn, and if so can
+     * the ai choose a pot that lands in that opponents tray so that the human cannot get
+     * another turn
+     */
+    @Test
+    public void testDefendAgainstAnotherTurn() {
+
+        for (int opponentsTray = 0; opponentsTray < 7; opponentsTray++) {
+            board = new Board();
+            for (int tray = 0; tray < 7; tray++) {
+                board.setTray(AI.PLAYER_AI, tray, 1);
+            }
+
+            board.setTray(AI.PLAYER_HUMAN, 0, 8);
+            board.setTray(AIStrategy.PLAYER_HUMAN, opponentsTray, 7 - opponentsTray);
+
+            for (int myTray = 0; myTray < 7; myTray++) {
+                int potsToTravel = (7 - myTray) + (opponentsTray + 1);
+                Random r = new Random();
+                board.setTray(AI.PLAYER_AI, myTray, potsToTravel + r.nextInt(7));
+
+                result = ai.defendAgainstAnotherTurn(board);
+                Assert.assertTrue(result.getX());
+                Assert.assertEquals(myTray, (int) result.getY());
+            }
+        }
+    }
+
+    /**
+     * Tests that the ai can pick a tray that has shells in
+     */
+    @Test
+    public void testRandomTray() {
+
+        for (int positionWithShells = 0; positionWithShells < 7; positionWithShells++) {
+            board = new Board();
+            for (int position = 0; position < 7; position++) {
+                if (positionWithShells != position) {
+                    board.setTray(AI.PLAYER_AI, position, 0);
+                }
+            }
+
+            int choice = ai.getRandomTray(board);
+            Assert.assertEquals(choice, positionWithShells);
+        }
+
+    }
+
+
+}
